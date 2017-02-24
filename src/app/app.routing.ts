adfe159ba0f32b20f5e3134f11c1c3b47304a69a
mofo-jones <jones@kodeinside.com>
2017-02-24 14:02:24 -0300

    First

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
+import { LoginComponent } from './my/login/login.component';
+
 export const routes: Routes = [
-  { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: '**', redirectTo: 'pages/dashboard' }
+    { path: 'admin', component: LoginComponent },
+    { path: '', redirectTo: 'pages/dashboard', pathMatch: 'full' },
+    { path: '**', redirectTo: 'pages/dashboard', pathMatch: 'full' },
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes, { useHash: true });
---
+++ src/app/globalVars.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// Parâmetro global com o endereço do host inicial para o sistema
+//export var host = 'http://localhost/ci4ng2/public/index.php/';
+export var host = 'http://localhost/kodeinside.com/index/index.php/';
+//export var host = 'http://kodeinside.com/ci4/public/index.php/';
+
+// Configuração de tradução para pt do componente prime calendar
+//export var pt = {
+//    firstDayOfWeek: 0,
+//    dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+//    dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+//    dayNamesMin: ["Do", "Se", "Te", "Qa", "Qi", "Se", "Sa"],
+//    monthNames: ["Janeiro", "Fevereito", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+//    monthNamesShort: ["Jan", "Fev", "Mac", "Abr", "Mai", "Jun", "Jul", "Aug", "Set", "Out", "Nov", "Dez"]
+//};
